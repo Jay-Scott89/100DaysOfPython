05a11495ec9a79666fc34074eacbcd7a94fa7b81
jamie.scott <jamie.scott89@live.co.uk>
2025-07-22 19:22:50 +0100

    Day 6, not much done

---
+++ Beginner/calling_functions.py
@@ -0,0 +1,18 @@
+print("Hello, World!") # This is a function. It may be build-in but its still a function at the end of the day. 
+
+# Define the function. 'def' 'name the function' 'arguments' 'close'
+def my_function():
+    print("Hello, World!")
+
+# Calling the function with arguments
+my_function()
+
+# Indentation matters! 
+
+# while looks keep going based on conditions
+# For looks such as range does for a set time. 
+
+something_is_true = True
+while something_is_true:
+    print("Hello")
+    something_is_true = False
